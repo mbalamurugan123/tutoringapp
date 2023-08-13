bc8608dd1ef289bda7ebfa164fead30f7f582214
mbalamurugan123 <727721euai011@skcet.ac.in>
2023-08-13 23:15:34 +0530

    second commit

--- src/Components/StudentRegester.js
+++ src/Components/StudentRegester.js
@@ -12,7 +12,6 @@ function StudentRegister() {
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
 
-
   const registerUser = async (email, password, name) => {
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, password);
